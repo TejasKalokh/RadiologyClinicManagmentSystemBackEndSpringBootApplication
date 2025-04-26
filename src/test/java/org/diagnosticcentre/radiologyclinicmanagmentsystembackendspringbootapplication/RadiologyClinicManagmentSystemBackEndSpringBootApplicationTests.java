adc1e32bd4ef9dcf5b06494d4a8ff50d52e41007
TejasKalokh <kalokhetejas3@gmail.com>
2025-04-26 18:40:54 +0530

    Initial Commit

---
+++ src/test/java/org/diagnosticcentre/radiologyclinicmanagmentsystembackendspringbootapplication/RadiologyClinicManagmentSystemBackEndSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.diagnosticcentre.radiologyclinicmanagmentsystembackendspringbootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadiologyClinicManagmentSystemBackEndSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
